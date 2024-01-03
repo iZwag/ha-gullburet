ece2ff86f87dab13693135b5bdddb60dfb388469
iZwag <jon.zk@hotmail.com>
2024-01-03 21:17:44 +0100

    Update hacs components

--- www/community/weather-card/weather-card.js
+++ www/community/weather-card/weather-card.js
@@ -297,15 +297,6 @@ class WeatherCard extends LitElement {
                       </div>
                     `
                   : ""}
-                ${!this._config.hide_precipitation &&
-                daily.precipitation_probability !== undefined &&
-                daily.precipitation_probability !== null
-                  ? html`
-                      <div class="precipitation_probability">
-                        ${Math.round(daily.precipitation_probability)} ${this.getUnit("precipitation_probability")}
-                      </div>
-                    `
-                  : ""}
               </div>
             `
           )}
